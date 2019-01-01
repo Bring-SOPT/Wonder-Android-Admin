edf4c1d2ba5fe607b8b6ab0b857cba6f41e1dd9e
Malibin <nightmare73@naver.com>
2019-01-01 23:52:45 +0900

    dialogs fixed

--- app/src/main/java/com/wonder/bring/wonderandroidowner/WaitingList/WaitingAcceptDialog.kt
+++ app/src/main/java/com/wonder/bring/wonderandroidowner/WaitingList/WaitingAcceptDialog.kt
@@ -1,25 +1,94 @@
 package com.wonder.bring.wonderandroidowner.WaitingList
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import com.wonder.bring.wonderandroidowner.R
 import kotlinx.android.synthetic.main.dialog_waiting_accept.*
 
-class WaitingAcceptDialog(ctx: Context):AlertDialog(ctx){
+class WaitingAcceptDialog(ctx: Context): Dialog(ctx){
+
+    var waitTime : Int = 0
+
+    val oneLineMessageDialog = OneLineMessageDialog(ctx,"주문 접수 알림이 전송되었습니다.")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_waiting_accept)
 
+        initOnClickListener()
+    }
+    private fun initOnClickListener(){
 
+        //5분 버튼
+        btn_waiting_accept_dialog_5m.setOnClickListener {
+            waitTime = 5
+
+            //버튼누르면 현재 다이얼로그 꺼지고 푸시알림 전송되었다는 다이얼로그 띄우기
+            dismiss()
+            oneLineMessageDialog.show()
+        }
+
+        //10분 버튼
+        btn_waiting_accept_dialog_10m.setOnClickListener {
+            waitTime = 10
+
+            //버튼누르면 현재 다이얼로그 꺼지고 푸시알림 전송되었다는 다이얼로그 띄우기
+            dismiss()
+            oneLineMessageDialog.show()
+        }
+
+        //15분 버튼
+        btn_waiting_accept_dialog_15m.setOnClickListener {
+            waitTime = 15
+
+            //버튼누르면 현재 다이얼로그 꺼지고 푸시알림 전송되었다는 다이얼로그 띄우기
+            dismiss()
+            oneLineMessageDialog.show()
+        }
+
+        //20분 버튼
+        btn_waiting_accept_dialog_20m.setOnClickListener {
+            waitTime = 20
+
+            //버튼누르면 현재 다이얼로그 꺼지고 푸시알림 전송되었다는 다이얼로그 띄우기
+            dismiss()
+            oneLineMessageDialog.show()
+        }
+
+        //25분 버튼
+        btn_waiting_accept_dialog_25m.setOnClickListener {
+            waitTime = 25
+
+            //버튼누르면 현재 다이얼로그 꺼지고 푸시알림 전송되었다는 다이얼로그 띄우기
+            dismiss()
+            oneLineMessageDialog.show()
+        }
+
+        //30분 버튼
+        btn_waiting_accept_dialog_30m.setOnClickListener {
+            waitTime = 30
+
+            //버튼누르면 현재 다이얼로그 꺼지고 푸시알림 전송되었다는 다이얼로그 띄우기
+            dismiss()
+            oneLineMessageDialog.show()
+        }
+
+        //취소버튼
         btn_waiting_accept_dialog_cancle.setOnClickListener {
             dismiss()
         }
 
+        //확인버튼
+        //직접 입력에 값이 없으면 동작하지 않게 해야한다.
         btn_waiting_accept_dialog_ok.setOnClickListener{
 
+            //버튼누르면 현재 다이얼로그 꺼지고 푸시알림 전송되었다는 다이얼로그 띄우기
             dismiss()
+            oneLineMessageDialog.show()
         }
     }
+
 }
