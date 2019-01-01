7a91c2ef4c07c4bfb025776eca5893da9b17b4d5
Malibin <nightmare73@naver.com>
2019-01-01 19:33:22 +0900

    watiing dialog edittext keyboard problem fixed

--- app/src/main/java/com/wonder/bring/wonderandroidowner/WaitingList/WaitingDenyDialog.kt
+++ app/src/main/java/com/wonder/bring/wonderandroidowner/WaitingList/WaitingDenyDialog.kt
@@ -1,12 +1,14 @@
 package com.wonder.bring.wonderandroidowner.WaitingList
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import com.wonder.bring.wonderandroidowner.R
+import com.wonder.bring.wonderandroidowner.R.id.btn_waiting_deny_dialog_ok
 import kotlinx.android.synthetic.main.dialog_waiting_deny.*
 
-class WaitingDenyDialog(ctx: Context):AlertDialog(ctx){
+class WaitingDenyDialog(ctx: Context): Dialog(ctx){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
