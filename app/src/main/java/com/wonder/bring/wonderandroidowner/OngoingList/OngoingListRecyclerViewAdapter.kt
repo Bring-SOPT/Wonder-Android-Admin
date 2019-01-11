483881ca006dd510cc95f226bfd5faeb62c55937
Malibin <nightmare73@naver.com>
2019-01-12 04:27:03 +0900

    파이어베이스 연동 제외 완료

--- app/src/main/java/com/wonder/bring/wonderandroidowner/OngoingList/OngoingListRecyclerViewAdapter.kt
+++ app/src/main/java/com/wonder/bring/wonderandroidowner/OngoingList/OngoingListRecyclerViewAdapter.kt
@@ -12,19 +12,32 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.wonder.bring.wonderandroidowner.MainActivity
+import com.wonder.bring.wonderandroidowner.Network.ApplicationController
+import com.wonder.bring.wonderandroidowner.Network.Get.GetChangeServerStatusResponseData
 import com.wonder.bring.wonderandroidowner.Network.Get.OrderListData
+import com.wonder.bring.wonderandroidowner.Network.NetworkService
 import com.wonder.bring.wonderandroidowner.R
 import com.wonder.bring.wonderandroidowner.R.drawable.waiting_order_border
 import com.wonder.bring.wonderandroidowner.SizeConvertor
 import com.wonder.bring.wonderandroidowner.WaitingList.OneLineMessageDialog
 import org.jetbrains.anko.backgroundColorResource
 import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class OngoingListRecyclerViewAdapter(val ctx: Context, val dataList: ArrayList<OrderListData>) :
     RecyclerView.Adapter<OngoingListRecyclerViewAdapter.Holder>() {
+
+    // 보미 서버 통신
+    val networkService: NetworkService by lazy {
+        ApplicationController.instance.networkService
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view: View = LayoutInflater.from(ctx).inflate(R.layout.rv_item_ongoing, parent, false)
         return Holder(view)
@@ -70,29 +83,104 @@ class OngoingListRecyclerViewAdapter(val ctx: Context, val dataList: ArrayList<O
 
         holder.btn_ready.setOnClickListener {
 
-            Log.v("Malibin Debug", "btn_ready position : $position")
-            customToast()
+            //서버통신 그냥 때려박음 ㅋㅋ시발------------------------------------------------------------------------
+            Log.v(
+                "Malibin Debug",
+                "내가 요청한 값들 :  storeIdx = " + MainActivity.storeIdx + " orderIdx = " + dataList[position].orderListIdx
+            )
 
-            holder.btn_ready.isEnabled = false
-            holder.btn_ready.backgroundResource = R.drawable.delivery_complete
+            networkService.getChangeServerStatus2Request(
+                "application/json",
+                MainActivity.storeIdx,
+                dataList[position].orderListIdx,
+                2 //이버튼 누를때는 항상 2로 바꾸는걸 요청
+            ).enqueue(object : Callback<GetChangeServerStatusResponseData> {
+                override fun onFailure(call: Call<GetChangeServerStatusResponseData>, t: Throwable) {
+                    ctx.toast("서버 통신에 실패하였습니다")
+                    Log.e("Malibin Debug",t.toString())
+                }
 
-            holder.btn_gave.isEnabled = true
-            holder.btn_gave.backgroundResource = R.drawable.waiting_order_border
+                override fun onResponse(
+                    call: Call<GetChangeServerStatusResponseData>,
+                    response: Response<GetChangeServerStatusResponseData>
+                ) {
+                    Log.v("Malibin Debug","응답은 왔는데 왜 안되냐? : "+response.body().toString())
+                    if (response.isSuccessful) {
 
-            dataList[position].state = 2
+                        var branch: Int = response.body()!!.status
+
+                        when (branch) {
+                            200 -> {
+                                Log.v("Malibin Debug", "서버 상태변경 성공")
+                                //상태변경 성공인경우
+                                customToast()
+
+                                holder.btn_ready.isEnabled = false
+                                holder.btn_ready.backgroundResource = R.drawable.delivery_complete
+
+                                holder.btn_gave.isEnabled = true
+                                holder.btn_gave.backgroundResource = R.drawable.waiting_order_border
+
+                                dataList[position].state = 2
+                            }
+                        }
+                    }
+                }
+
+            })
+            //서버통신 그냥 때려박음 ㅋㅋ시발------------------------------------------------------------------------
+
         }
 
         holder.btn_gave.setOnClickListener {
 
+            //서버통신 그냥 때려박음 ㅋㅋ시발------------------------------------------------------------------------
+            Log.v(
+                "Malibin Debug",
+                "내가 요청한 값들 :  storeIdx = " + MainActivity.storeIdx + " orderIdx = " + dataList[position].orderListIdx
+            )
+
+            networkService.getChangeServerStatus2Request(
+                "application/json",
+                MainActivity.storeIdx,
+                dataList[position].orderListIdx,
+                3 //이 버튼 누를때는 항상 3으로 바꾸는 걸 요청
+            ).enqueue(object : Callback<GetChangeServerStatusResponseData> {
+                override fun onFailure(call: Call<GetChangeServerStatusResponseData>, t: Throwable) {
+                    ctx.toast("서버 통신에 실패하였습니다")
+                    Log.e("Malibin Debug",t.toString())
+                }
+
+                override fun onResponse(
+                    call: Call<GetChangeServerStatusResponseData>,
+                    response: Response<GetChangeServerStatusResponseData>
+                ) {
+                    Log.v("Malibin Debug","응답은 왔는데 왜 안되냐? : "+response.body().toString())
+                    if (response.isSuccessful) {
+
+                        var branch: Int = response.body()!!.status
+
+                        when (branch) {
+                            200 -> {
+                                Log.v("Malibin Debug", "서버 상태변경 성공")
+                                //상태변경 성공인경우
+                                (ctx as MainActivity).callDoneAddRVItem(dataList[position])
+
+                                dataList.removeAt(position)
+                                notifyItemRemoved(position)
+                                notifyItemRangeChanged(position, dataList.size)
+                                //notifyItemChanged(position)
+                                //notifyDataSetChanged()
+                            }
+                        }
+                    }
+                }
+
+            })
+            //서버통신 그냥 때려박음 ㅋㅋ시발------------------------------------------------------------------------
             //메인액티비티에 있는 callDoneAddRVItem 메소드를 호출
             //그럼 액티비티에서는 DoneFragment에 있는 addRVItem 함수를 호출함
-            (ctx as MainActivity).callDoneAddRVItem(dataList[position])
 
-            dataList.removeAt(position)
-            notifyItemRemoved(position)
-            notifyItemRangeChanged(position, dataList.size)
-            //notifyItemChanged(position)
-            //notifyDataSetChanged()
 
 
             Log.v("Malibin Debug", "btn_gave position : $position")
@@ -133,5 +221,4 @@ class OngoingListRecyclerViewAdapter(val ctx: Context, val dataList: ArrayList<O
         dataList.add(item)
         notifyItemInserted(positon)
     }
-
 }
